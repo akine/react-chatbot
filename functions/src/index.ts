e6f60be7ed0a3d7744e05de28bb02294a7d6dadf
akine <weezer.and.you@gmail.com>
2020-05-19 08:49:04 +0900

    cloudfunctions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,24 @@
-// import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions';
+import * as admin from "firebase-admin";
+admin.initializeApp();
+const db = admin.firestore();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const sendResponse = (response: functions.Response, statusCode: number, body: any) => {
+  response.send( {
+    statusCode,
+    body: JSON.stringify(body)
+  })
+}
+
+export const addDataset = functions.https.onRequest(async (req: any, res: any) => {
+  if (req.method !== 'POST') {
+    sendResponse(res, 405, {error: 'Invalid Request'})
+  } else {
+    const dataset = req.body
+    for (const key of Object.keys(dataset)) {
+      const data = dataset[key]
+      await db.collection( 'questions' ).doc(key).set(data)
+    }
+    sendResponse(req, 200, {message: 'Successfully added dataset!'})
+  }
+})
