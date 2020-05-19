371b177e35eb80ab760f3be339cb881c95113505
akine <weezer.and.you@gmail.com>
2020-05-19 09:28:53 +0900

    firebase deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import defaultDataset from './dataset';
 import './assets/styles/style.css';
 import { AnswersList, Chats } from './components'
 import FormDialog from './components/Forms/FormDialog';
+import {db} from './firebase/index';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -11,7 +11,7 @@ export default class App extends React.Component {
       answers: [],
       chats: [],
       currentId: "init",
-      dataset: defaultDataset,
+      dataset: {},
       open: false
     }
     this.selectAnswer = this.selectAnswer.bind(this)
@@ -79,9 +79,26 @@ export default class App extends React.Component {
     this.setState({ open: false });
   };
 
+  initDataset = (dataset) => {
+    this.setState({dataset: dataset})
+  }
+
   componentDidMount() {
-    const initAnswer = "";
-    this.selectAnswer(initAnswer, this.state.currentId)
+    (async() => {
+      const dataset = this.state.dataset
+      
+      await db.collection('questions').get().then(snapshots => {
+        snapshots.forEach(doc => {
+          const id = doc.id
+          const data = doc.data()
+          dataset[id] = data
+        })
+      })
+
+      this.initDataset(dataset)
+      const initAnswer = "";
+      this.selectAnswer(initAnswer, this.state.currentId)
+    })()
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCu5JdpaGz0LKP-trdIz-CiwwQbLeQsbPg",
+  authDomain: "react-chatbot-58aed.firebaseapp.com",
+  databaseURL: "https://react-chatbot-58aed.firebaseio.com",
+  projectId: "react-chatbot-58aed",
+  storageBucket: "react-chatbot-58aed.appspot.com",
+  messagingSenderId: "745096503626",
+  appId: "1:745096503626:web:55badd050f3349ab6ee1b3",
+  measurementId: "G-02MZ48V956"
+};
+
+export default firebaseConfig
