059ec23340f8c3320d8bfc754228663942bc484a
akine <weezer.and.you@gmail.com>
2020-05-19 09:00:12 +0900

    fix cloudfunctions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,21 +4,21 @@ admin.initializeApp();
 const db = admin.firestore();
 
 const sendResponse = (response: functions.Response, statusCode: number, body: any) => {
-  response.send( {
+  response.send({
     statusCode,
     body: JSON.stringify(body)
-  })
-}
+  });
+};
 
 export const addDataset = functions.https.onRequest(async (req: any, res: any) => {
   if (req.method !== 'POST') {
-    sendResponse(res, 405, {error: 'Invalid Request'})
+    sendResponse(res, 405, { error: "Invalid Request" })
   } else {
-    const dataset = req.body
+    const dataset = req.body;
     for (const key of Object.keys(dataset)) {
-      const data = dataset[key]
-      await db.collection( 'questions' ).doc(key).set(data)
+      const data = dataset[key];
+      await db.collection('questions').doc(key).set(data)
     }
-    sendResponse(req, 200, {message: 'Successfully added dataset!'})
+    sendResponse(res, 200, { message: 'Successfully added dataset! WooHoo!' });
   }
-})
+});
