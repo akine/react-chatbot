ae51ff12c91c09d3e2c24ee8f5edef50a22c09b3
akine <weezer.and.you@gmail.com>
2020-05-19 20:02:53 +0900

    fix slack, gitignore

--- src/components/Forms/FormDialog.jsx
+++ src/components/Forms/FormDialog.jsx
@@ -5,6 +5,7 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import TextInput from './TextInput';
+import webhookConfig from '../../webhookConfig'
 
 export default class FormDialog extends React.Component {
   // const[open, setOpen] = React.useState(false);
@@ -43,11 +44,9 @@ export default class FormDialog extends React.Component {
         '名前:' + name + '\n' +
         'Email:' + email + '\n' +
         'お問い合わせ内容:\n' + description
-    }
+    };
 
-    const url = 'https://hooks.slack.com/services/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'
-
-    fetch(url, {
+    fetch(webhookConfig, {
       method: 'POST',
       body: JSON.stringify(payload)
     }).then(() => {
@@ -60,7 +59,7 @@ export default class FormDialog extends React.Component {
       return this.props.handleClose()
     })
 
-  }
+  };
 
   render() {
     return (
